ed2b05f602f4ec152ab862a83e64d0a656967995
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-23 22:14:10 -0300

    exemplifying the concepts of each type of data

---
+++ implementing_data_types.py
@@ -0,0 +1,9 @@
+height = 1.85
+text = "Hello World"
+raining = True
+flavor_cake = "cornmeal"
+soap_units = 1
+
+print(height, text, raining, flavor_cake, soap_units)
+
+# 1.85 Hello World True cornmeal 1
