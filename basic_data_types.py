0f58abcd9d281720d3c608072ad11c00735b9839
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-23 22:06:34 -0300

    basic data types in python

---
+++ basic_data_types.py
@@ -0,0 +1,13 @@
+print(type(2)); # 2 is an integer, so it's an integer data type.
+print(type(2.5)); # 2.5 isn't an integer. it's what we call a floating point, rational numbers, decimals.
+print(type(1.5+1.5j)) # the type of number 1 + 1j (j is the imaginary number) is complex.
+print(type("Hello World")) # string to store text, characters.
+print(type(True)) # boolean type for True and False. it represents the logical value of the proposition.
+
+# then, the following sequence is expected:
+
+# <class 'int'>
+# <class 'float'>
+# <class 'complex'>
+# <class 'str'>
+# <class 'bool'>
