1a3229406635a8071521d8356dda865bcaa0ed77
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-25 20:17:40 -0300

    celsius to fahrenheit converter

---
+++ temperature_converter.py
@@ -0,0 +1,5 @@
+# Conversor de Celsius(°C) para Fahrenheit(°F)
+
+temperatura_celsius = float(input("Informe a temperatura em °C: "))
+temperatura_fahrenheit = 32 + (9*temperatura_celsius/5);
+print(f"{temperatura_celsius}°C equivale a {temperatura_fahrenheit}°F");
