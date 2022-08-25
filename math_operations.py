3675388bd70d76e94825d1b61bdd78e2840b3f18
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-25 12:11:26 -0300

    basic math operations in python

---
+++ math_operations.py
@@ -0,0 +1,8 @@
+print(2.5+5j + 1) # '+' usual sum : 3.5 + 5j
+print(10 - 1.5) # '-' usual subtraction : 8.5
+print(2 * -2) # '*' usual multiplication : -4
+print(82 / 4) # '/' usual division : 20.5
+
+print(4 ** -0.5) # '**' exponentiation : 0.5
+print( 9// 3) # '//' quotient of the integer division : 3
+print(9 % 3) # '%' remainder of the integer division : 0
