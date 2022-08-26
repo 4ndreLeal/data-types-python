a8d7bdac6bdaaf27f123991f9e563e4ba1a9491a
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-26 18:28:20 -0300

    dollar to real money converter

---
+++ currency_converter.py
@@ -0,0 +1,5 @@
+# Conversor de Dólar(USD) para Real(BRL)
+# 1 USD = 5.06 BRL
+dolar = float(input("Informe um valor em USD: "))
+real = dolar * 5.06
+print(f"{dolar} doláres está valendo {real} reais")
