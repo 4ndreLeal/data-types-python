cfe8325ea573e42807039e5e710efe80180471d2
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-25 21:02:43 -0300

    inches to centimeters converter

---
+++ unit_converter.py
@@ -0,0 +1,5 @@
+# Conversão de Polegadas(in) para Centímetros(cm)
+# 1 in = 2.54cm
+polegadas = float(input("Informe a medida em polegadas: "))
+centimetros = polegadas*2.54
+print(f"{polegadas} polegadas equivale a {centimetros} centímetros")
